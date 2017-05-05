dd3450013b1ec5389b1cb4ce8f3372add8764277
Luke Woodward <luke.wdwrd@gmail.com>
2017-05-05 14:19:02 -0500

    config database connection

---
+++ knexfile.js
@@ -0,0 +1,42 @@
+// Update with your config settings.
+
+module.exports = {
+
+  test: {
+    client: 'postgresql',
+    connection: {
+      database: 'buzzpoint'
+    },
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds'
+    }
+  },
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'buzzpoint'
+    },
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds'
+    }
+  },
+
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds'
+    }
+  }
+};
